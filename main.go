2e8c9cfd74daf140185459b862bb608155e2b36d
Yacine <yacinebenarousi@gmail.com>
2026-08-19 04:04:48 +0100

    feat(peers): bittorentHandshake, loop

--- main.go
+++ main.go
@@ -7,52 +7,88 @@ import (
 	"torrent/utils"
 )
 
-func main()  {
-	file, err := os.Open("The First Berserker - Khazan [FitGirl Repack].torrent")
-
-	if err!= nil {
-		log.Fatal(err)
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Printf("USAGE: torrent <torrent file> <download path>\n")
+		return
 	}
 
+	torrentFile := os.Args[1]
+	downloadPath := os.Args[2]
+
+	if torrentFile == "" {
+		fmt.Printf("select a torrent file\n")
+		return
+	}
+
+	if downloadPath == "" {
+		fmt.Printf("select a download path\n")
+		return
+	}
+
+	file, err := os.Open(torrentFile)
+	if err != nil {
+		log.Fatal(err)
+	}
 	defer file.Close()
 
 	fileInfo, err := file.Stat()
-	if err!= nil {
+	if err != nil {
 		log.Fatal(err)
 	}
 	fileSize := fileInfo.Size()
 	data := make([]byte, fileSize)
 	_, err = file.Read(data)
-
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	pos := 0
 	metadata, err := utils.BuildMetaData(data, pos)
-
-
-	// for i := range metadata.Announce_list{
-	// 	peer, err := utils.ConnectToTracker(metadata, i)
-	// 	if err!=nil{
-	// 		fmt.Printf("error %s\n", err)
-	// 		continue
-	// 	}
-	// 	fmt.Printf("Peers from announce %d :\n",i)
-	// 	for j := range peer{
-	// 		fmt.Printf("peer %d: %s:%s", j,peer[j].Ip, peer[j].Port)
-	// 	}
-		
-	// }
-
+	if err != nil {
+		log.Fatalf("failed to parse torrent: %s", err)
+	}
 
 	peers := utils.ConnectTrackersAsync(metadata)
-	
+	fmt.Printf("Found %d peers\n", len(peers))
+
 	for i, peer := range peers {
 		fmt.Printf("peer %d: %s %s\n", i, peer.Ip, peer.Port)
 	}
 
-	peerConn, err := utils.ConnectToPeer(peers)
-	if err!=nil{
-		fmt.Printf("err: %s\n", err)
+	if len(peers) == 0 {
+		fmt.Println("No peers found, giving up")
+		return
 	}
 
-	fmt.Printf("%s %s\n", peerConn.Peer.Ip, peerConn.Peer.Port)
+	peerConn, err := utils.ConnectToPeer(peers)
+	if err != nil {
+		fmt.Printf("Failed to connect to peer: %s\n", err)
+		return
+	}
+
+	fmt.Printf("Connected to peer: %s:%s\n", peerConn.Peer.Ip, peerConn.Peer.Port)
+
+	_, state, err := utils.BitTorrentHandShake(&peerConn, metadata)
+	if err != nil {
+		fmt.Printf("Handshake error: %s\n", err)
+		peerConn.Conn.Close()
+		return
+	}
+
+	if !state {
+		fmt.Printf("Handshake failed!\n")
+		peerConn.Conn.Close()
+		return
+	}
+
+	fmt.Println("Handshake successful!")
+
+	numPieces := len(metadata.Info.Pieces)
+	piecesCheck := make(map[int]bool, numPieces)
+	for i := 0; i < numPieces; i++ {
+		piecesCheck[i] = false
+	}
+
+	utils.BitLoop(&peerConn, metadata, piecesCheck, downloadPath)
 }
--- utils/peers.go
+++ utils/peers.go
@@ -1,18 +1,41 @@
 package utils
 
 import (
+	"bytes"
+	"crypto/sha1"
+	"encoding/binary"
 	"fmt"
+	"io"
+	"math/rand/v2"
 	"net"
+	"os"
 	"sync"
 	"time"
+	"torrent/core"
 )
 
+var peerID = "-TR0001-" + fmt.Sprintf("%012d", rand.N(1000000000000))
+
 type PeerConn struct {
-	Conn net.Conn
-	Peer PeerNode
+	Conn   net.Conn
+	Peer   PeerNode
+	PeerID string
 }
 
-func ConnectToPeer(peers []PeerNode) (PeerConn, error){ 
+type PeerState int
+
+const (
+	Choke PeerState = 0
+	Unchoke PeerState = 1
+	Interested PeerState = 2
+	NotInterested PeerState = 3
+	Have PeerState = 4
+	Bitfield PeerState = 5
+	Request PeerState = 6
+	Piece PeerState = 7
+)
+
+func ConnectToPeer(peers []PeerNode) (PeerConn, error) {
 	var wg sync.WaitGroup
 	var mutex sync.Mutex
 
@@ -22,20 +45,19 @@ func ConnectToPeer(peers []PeerNode) (PeerConn, error){
 	for _, peer := range peers {
 		wg.Add(1)
 
-		go func(p PeerNode){
+		go func(p PeerNode) {
 			defer wg.Done()
 
 			conn, err := net.DialTimeout("tcp", net.JoinHostPort(p.Ip, p.Port), 5*time.Second)
-			if err!=nil{
-				fmt.Printf("error: %s\n",err)
-				return 
+			if err != nil {
+				fmt.Printf("error: %s\n", err)
+				return
 			}
 
 			mutex.Lock()
 			defer mutex.Unlock()
 
-			if wonRace{
-				// fmt.Printf("%s lost the race!\n", p.Ip)
+			if wonRace {
 				conn.Close()
 				return
 			}
@@ -45,15 +67,363 @@ func ConnectToPeer(peers []PeerNode) (PeerConn, error){
 				Peer: p,
 				Conn: conn,
 			}
-			// fmt.Printf("%s won the race!\n",p.Ip)
 		}(peer)
-		
 	}
 
 	wg.Wait()
 
-	if !wonRace{
-		return PeerConn{}, fmt.Errorf("no peers could be connected!\n")
+	if !wonRace {
+		return PeerConn{}, fmt.Errorf("no peers could be connected")
 	}
 	return peerConn, nil
+}
+
+
+func ConnectAndHandshake(torrent *core.TorrentMetaData) (*PeerConn, error) {
+	peers := ConnectTrackersAsync(torrent)
+
+	peerConn, err := ConnectToPeer(peers)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to peer: %w", err)
+	}
+
+	_, ok, err := BitTorrentHandShake(&peerConn, torrent)
+	if err != nil {
+		peerConn.Conn.Close()
+		return nil, fmt.Errorf("handshake error: %w", err)
+	}
+	if !ok {
+		peerConn.Conn.Close()
+		return nil, fmt.Errorf("handshake rejected by peer")
+	}
+
+	return &peerConn, nil
+}
+
+func BitTorrentHandShake(peer *PeerConn, torrent *core.TorrentMetaData) (*net.Conn, bool, error) {
+
+	h := sha1.New()
+	h.Write(torrent.RawInfoBytes)
+	info_hash := h.Sum(nil)
+
+	handshake := make([]byte, 68)
+	handshake[0] = 19
+	copy(handshake[1:20], []byte("BitTorrent protocol"))
+	copy(handshake[28:48], info_hash)
+	copy(handshake[48:68], []byte(peerID))
+
+	n, err := peer.Conn.Write(handshake)
+	if err != nil {
+		return nil, false, fmt.Errorf("%s", err)
+	}
+
+	if n != 68 {
+		return nil, false, fmt.Errorf("incomplete handshake write: wrote %d bytes", n)
+	}
+
+	response := make([]byte, 68)
+
+	rn, err := io.ReadFull(peer.Conn, response)
+	if err != nil {
+		return nil, false, fmt.Errorf("handshake read failed after %d bytes: %w", rn, err)
+	}
+
+	headerCheck := int(response[0]) == 19
+	protocolCheck := string(response[1:20]) == "BitTorrent protocol"
+	infoHashCheck := bytes.Equal(response[28:48], info_hash)
+
+	if headerCheck && protocolCheck && infoHashCheck {
+		peer.PeerID = string(response[48:68])
+		return &peer.Conn, true, nil
+	}
+
+	return nil, false, fmt.Errorf("handshake failed: header=%v, protocol=%v, info_hash=%v",
+		headerCheck, protocolCheck, infoHashCheck)
+}
+
+
+func BitLoop(peerConn *PeerConn, torrent *core.TorrentMetaData, piecesCheck map[int]bool, downloadPath string) {
+	receivedBlocks := make(map[int]map[int]bool)
+
+	reconnect := func(reason string) bool {
+		fmt.Printf("%s reconnecting to a new peer\n", reason)
+		peerConn.Conn.Close()
+
+		newPeer, err := ConnectAndHandshake(torrent)
+		if err != nil {
+			fmt.Printf("failed to reconnect: %s\n", err)
+			return false
+		}
+
+		*peerConn = *newPeer
+		receivedBlocks = make(map[int]map[int]bool)
+		return true
+	}
+
+
+	const stallTimeout = 120 * time.Second
+
+	for {
+		peerConn.Conn.SetReadDeadline(time.Now().Add(stallTimeout))
+
+		messageLen := make([]byte, 4)
+		_, err := io.ReadFull(peerConn.Conn, messageLen)
+		if err != nil {
+			if !reconnect(fmt.Sprintf("Connection error: %s", err)) {
+				return
+			}
+			continue
+		}
+
+		length := binary.BigEndian.Uint32(messageLen)
+		fmt.Printf("received message length: %d\n", length)
+
+		if length == 0 {
+			continue
+		}
+
+		message := make([]byte, length)
+		_, err = io.ReadFull(peerConn.Conn, message)
+		if err != nil {
+			if !reconnect(fmt.Sprintf("Failed to read message: %s", err)) {
+				return
+			}
+			continue
+		}
+
+		messageId := message[0]
+		payload := message[1:]
+		fmt.Printf("message ID: %d (payload size: %d bytes)\n", messageId, len(payload))
+
+		switch messageId {
+		case byte(Choke):
+			if !reconnect("Got Choke") {
+				return
+			}
+
+		case byte(Request):
+			fmt.Println("peer requested a piece")
+			pieceIdx := binary.BigEndian.Uint32(payload[0:4])
+			if piecesCheck[int(pieceIdx)] {
+				sendPiece(peerConn, int(pieceIdx), downloadPath)
+			}
+
+		case byte(Bitfield):
+			fmt.Println("received bitfield")
+			hasPiecesINeed := false
+			numPieces := len(torrent.Info.Pieces)
+
+			for i := 0; i < len(payload)*8 && i < numPieces; i++ {
+				byteIndex := i / 8
+				bitIndex := uint(i % 8)
+				hasPiece := (payload[byteIndex] >> (7 - bitIndex)) & 1
+
+				if hasPiece == 1 && !piecesCheck[i] {
+					hasPiecesINeed = true
+					break
+				}
+			}
+
+			if hasPiecesINeed {
+				sendInterested(peerConn)
+				fmt.Println("sent Interested!")
+			} else {
+				sendNotInterested(peerConn)
+				fmt.Println("sent Not Interested")
+			}
+
+		case byte(Unchoke):
+			fmt.Println("got unchoke, requesting one piece")
+			numPieces := len(torrent.Info.Pieces)
+			for i := 0; i < numPieces; i++ {
+				if !piecesCheck[i] {
+					sendRequest(torrent, peerConn, i, downloadPath)
+					break
+				}
+			}
+
+		case byte(Have):
+			pieceIndex := int(binary.BigEndian.Uint32(payload[0:4]))
+			fmt.Printf("peer has piece %d\n", pieceIndex)
+			if !piecesCheck[pieceIndex] {
+				sendRequest(torrent, peerConn, pieceIndex, downloadPath)
+			}
+
+		case byte(Piece):
+			pieceIndex := int(binary.BigEndian.Uint32(payload[0:4]))
+			begin := int(binary.BigEndian.Uint32(payload[4:8]))
+			data := payload[8:]
+
+			fmt.Printf("received piece %d block at offset %d (%d bytes)\n", pieceIndex, begin, len(data))
+
+			if piecesCheck[pieceIndex] {
+				sendNotInterested(peerConn)
+				break
+			}
+
+			err := os.WriteFile(fmt.Sprintf("%s/piece_%d_block_%d", downloadPath, pieceIndex, begin), data, 0644)
+			if err != nil {
+				fmt.Printf("error saving block: %s\n", err)
+				break
+			}
+			fmt.Printf("saved block for piece %d at offset %d\n", pieceIndex, begin)
+
+			if receivedBlocks[pieceIndex] == nil {
+				receivedBlocks[pieceIndex] = make(map[int]bool)
+			}
+			receivedBlocks[pieceIndex][begin] = true
+
+			pieceLength := int(torrent.Info.Piece_length)
+			blockSize := 16384
+			totalBlocks := (pieceLength + blockSize - 1) / blockSize
+
+			if len(receivedBlocks[pieceIndex]) < totalBlocks {
+				sendRequest(torrent, peerConn, pieceIndex, downloadPath)
+				break
+			}
+
+			fmt.Printf("piece %d is complete\n", pieceIndex)
+
+			fullPiece := []byte{}
+			blockReadFailed := false
+			for offset := 0; offset < pieceLength; offset += blockSize {
+				blockData, err := os.ReadFile(fmt.Sprintf("%s/piece_%d_block_%d", downloadPath, pieceIndex, offset))
+				if err != nil {
+					fmt.Printf("missing block at offset %d\n", offset)
+					blockReadFailed = true
+					break
+				}
+				fullPiece = append(fullPiece, blockData...)
+			}
+			if blockReadFailed {
+				break
+			}
+
+			h := sha1.New()
+			h.Write(fullPiece)
+			hash := h.Sum(nil)
+
+			expectedHash := torrent.Info.Pieces[pieceIndex]
+
+			if !bytes.Equal(hash, expectedHash[:]) {
+				fmt.Printf("piece %d verification failed, re-requesting...\n", pieceIndex)
+				delete(receivedBlocks, pieceIndex)
+				sendRequest(torrent, peerConn, pieceIndex, downloadPath)
+				break
+			}
+
+			fmt.Printf("piece %d verified!\n", pieceIndex)
+
+			err = os.WriteFile(fmt.Sprintf("%s/piece_%d", downloadPath, pieceIndex), fullPiece, 0644)
+			if err != nil {
+				fmt.Printf("error saving piece: %s\n", err)
+				break
+			}
+
+			piecesCheck[pieceIndex] = true
+			sendHave(peerConn, pieceIndex)
+			delete(receivedBlocks, pieceIndex)
+
+			numPieces := len(torrent.Info.Pieces)
+			for i := 0; i < numPieces; i++ {
+				if !piecesCheck[i] {
+					fmt.Printf("requesting next piece: %d\n", i)
+					sendRequest(torrent, peerConn, i, downloadPath)
+					break
+				}
+			}
+
+			allDone := true
+			for i := 0; i < numPieces; i++ {
+				if !piecesCheck[i] {
+					allDone = false
+					break
+				}
+			}
+			if allDone {
+				fmt.Println("all pieces downloaded!")
+				return
+			}
+
+		default:
+			fmt.Printf("Unknown message ID: %d\n", messageId)
+		}
+	}
+}
+
+func sendRequest(torrent *core.TorrentMetaData, peerConn *PeerConn, pieceIdx int, downloadPath string) {
+	blockSize := 16384
+	pieceLength := int(torrent.Info.Piece_length)
+
+	for begin := 0; begin < pieceLength; begin += blockSize {
+
+		blockFile := fmt.Sprintf("%s/piece_%d_block_%d", downloadPath, pieceIdx, begin)
+		if _, err := os.Stat(blockFile); err == nil {
+			continue
+		}
+
+		actualSize := blockSize
+		if begin+blockSize > pieceLength {
+			actualSize = pieceLength - begin
+		}
+
+		msg := make([]byte, 17)
+		binary.BigEndian.PutUint32(msg[0:4], 13)
+		msg[4] = byte(Request)
+		binary.BigEndian.PutUint32(msg[5:9], uint32(pieceIdx))
+		binary.BigEndian.PutUint32(msg[9:13], uint32(begin))
+		binary.BigEndian.PutUint32(msg[13:17], uint32(actualSize))
+		peerConn.Conn.Write(msg)
+		fmt.Printf("requested piece %d block at offset %d\n", pieceIdx, begin)
+
+		break
+	}
+}
+
+func sendInterested(peerConn *PeerConn) {
+	msg := make([]byte, 5)
+	binary.BigEndian.PutUint32(msg[0:4], 1)
+	msg[4] = byte(Interested)
+	peerConn.Conn.Write(msg)
+}
+
+func sendNotInterested(peerConn *PeerConn) {
+	msg := make([]byte, 5)
+	binary.BigEndian.PutUint32(msg[0:4], 1)
+	msg[4] = byte(NotInterested)
+	peerConn.Conn.Write(msg)
+}
+
+func sendPiece(peerConn *PeerConn, pieceIndex int, filePath string) {
+	data, err := os.ReadFile(fmt.Sprintf("%s/piece_%d", filePath, pieceIndex))
+	if err != nil {
+		return
+	}
+
+	blockSize := 16384
+
+	for begin := 0; begin < len(data); begin += blockSize {
+		end := begin + blockSize
+		if end > len(data) {
+			end = len(data)
+		}
+		block := data[begin:end]
+
+		msg := make([]byte, 13+len(block))
+		binary.BigEndian.PutUint32(msg[0:4], uint32(9+len(block)))
+		msg[4] = byte(Piece)
+		binary.BigEndian.PutUint32(msg[5:9], uint32(pieceIndex))
+		binary.BigEndian.PutUint32(msg[9:13], uint32(begin))
+		copy(msg[13:], block)
+		peerConn.Conn.Write(msg)
+	}
+}
+
+func sendHave(peerConn *PeerConn, pieceIndex int) {
+	msg := make([]byte, 9)
+	binary.BigEndian.PutUint32(msg[0:4], 5)
+	msg[4] = byte(Have)
+	binary.BigEndian.PutUint32(msg[5:9], uint32(pieceIndex))
+	peerConn.Conn.Write(msg)
+	fmt.Printf("sent Have for piece %d\n", pieceIndex)
 }
